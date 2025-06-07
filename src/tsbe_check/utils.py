b4f329d220a3d75a0392802cbba2714742e5d43d
Abhi-Bohora <abhishek.bohora18@gmail.com>
2025-06-07 21:20:55 +0545

    format

--- src/tsbe_check/utils.py
+++ src/tsbe_check/utils.py
@@ -19,17 +19,17 @@ def snake_to_camel(input_str: str) -> str:
     str
         The camelCase version of the input string.
     """
-    if not input_str or input_str.startswith('_'):
+    if not input_str or input_str.startswith("_"):
         return input_str
-        
-    words = input_str.split('_')
+
+    words = input_str.split("_")
     result = words[0].lower()
-    
+
     for word in words[1:]:
         if word:
             if any(c.isupper() for c in word[1:]):
                 result += word[0].upper() + word[1:]
             else:
                 result += word[0].upper() + word[1:].lower()
-    
+
     return result
