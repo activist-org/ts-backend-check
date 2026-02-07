fa93a742d8770a6dfe9cb6b892ce0a7add300521
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2026-02-07 18:32:20 +0100

    Minor updates to yaml and contributing mentorship section

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,36 +4,6 @@ import pytest
 
 from ts_backend_check.cli.main import config
 
-# MARK: Invalid
-
-invalid_django_models = config["invalid_model"]["backend_model_path"]
-invalid_ts_interfaces = config["invalid_model"]["ts_interface_path"]
-invalid_check_blank_models = config["invalid_model"]["check_blank_model_fields"]
-invalid_backend_to_ts_conversions = config["invalid_model"][
-    "backend_to_ts_model_name_conversions"
-]
-
-
-@pytest.fixture
-def return_invalid_django_models():
-    return str(invalid_django_models)
-
-
-@pytest.fixture
-def return_invalid_ts_interfaces():
-    return str(invalid_ts_interfaces)
-
-
-@pytest.fixture
-def return_invalid_check_blank_models():
-    return invalid_check_blank_models
-
-
-@pytest.fixture
-def return_invalid_backend_to_ts_conversions():
-    return invalid_backend_to_ts_conversions
-
-
 # MARK: Valid
 
 valid_django_models = config["valid_model"]["backend_model_path"]
@@ -62,3 +32,33 @@ def return_valid_check_blank_models():
 @pytest.fixture
 def return_valid_backend_to_ts_conversions():
     return valid_backend_to_ts_conversions
+
+
+# MARK: Invalid
+
+invalid_django_models = config["invalid_model"]["backend_model_path"]
+invalid_ts_interfaces = config["invalid_model"]["ts_interface_path"]
+invalid_check_blank_models = config["invalid_model"]["check_blank_model_fields"]
+invalid_backend_to_ts_conversions = config["invalid_model"][
+    "backend_to_ts_model_name_conversions"
+]
+
+
+@pytest.fixture
+def return_invalid_django_models():
+    return str(invalid_django_models)
+
+
+@pytest.fixture
+def return_invalid_ts_interfaces():
+    return str(invalid_ts_interfaces)
+
+
+@pytest.fixture
+def return_invalid_check_blank_models():
+    return invalid_check_blank_models
+
+
+@pytest.fixture
+def return_invalid_backend_to_ts_conversions():
+    return invalid_backend_to_ts_conversions
